db2caca5fe9392fcc8a507257c10635d39f6d75f
dangthemen10 <phanhaidangkgumt@gmail.com>
2021-02-22 00:06:45 +0700

    Them giao dien

--- src/main/java/com/techbase/practicespringboot/controller/StudentController.java
+++ src/main/java/com/techbase/practicespringboot/controller/StudentController.java
@@ -3,13 +3,14 @@ package com.techbase.practicespringboot.controller;
 import com.techbase.practicespringboot.entity.StudentEntity;
 import com.techbase.practicespringboot.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@RestController
+@Controller
 public class StudentController {
     @Autowired
     private StudentService studentService;
@@ -28,7 +29,7 @@ public class StudentController {
 
     @GetMapping("/student/{id}/edit")
     public String edit(@PathVariable int id, Model model) {
-        model.addAttribute("employee", studentService.findOneStudent(id));
+        model.addAttribute("student", studentService.findOneStudent(id));
         return "form";
     }
 
